d1cee53ca29b3695e88d1730ef156a9b402cb2b9
wangmeng <wangmeng@ysstech.com>
2024-09-01 18:40:59 +0800

    苍穹外卖后端初始代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,33 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("新增员时传递的数据模型")
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty("员工id")
     private Long id;
 
+    @ApiModelProperty("用户名")
     private String username;
 
+    @ApiModelProperty("姓名")
     private String name;
 
+    @ApiModelProperty("手机号")
     private String phone;
 
+    @ApiModelProperty("性别")
     private String sex;
 
+    @ApiModelProperty("身份证")
     private String idNumber;
 
 }
